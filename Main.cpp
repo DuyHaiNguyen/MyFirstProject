1c96f642e2b05b6819368b6a7b5bb59cfb2fc553
DuyHaiNguyen <89535755+DuyHaiNguyen@users.noreply.github.com>
2021-08-26 10:39:47 -0500

    Add files via upload

---
+++ Main.cpp
@@ -0,0 +1,30 @@
+/*
+Duy Nguyen
+C++ 2021 FA
+Due:
+Lab 1 Introduction to C++ Programming
+Description:	This lab will serve as a demonstration of your ability to download a project from GitHub, compile and run the project, upload a project to GitHub, 
+				and submit a project to Google Classroom. 
+				This lab assumes you have downloaded and installed an IDE (integrated development environment) and have also installed GitHub Desktop.
+				If you have not met the assumptions for the lab, the steps you need to complete the lab may vary from what is here.
+				The goals are for you to download a project, compile and run that project, and to upload your own project. 
+				If you meet the goals, you will get the points for this lab.
+*/
+#include <iostream>;
+
+#include <stdlib.h>;
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello World!" << endl;
+
+	cout << "C++ is a great programming language to start with" << endl;
+
+	system("color 0B");
+
+	system("pause");
+
+	return 0;
+}
